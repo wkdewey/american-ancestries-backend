ef8a91c547e7d58d9359805ee6b2c901b62105cc
wkdewey <wkdewey@gmail.com>
2020-10-13 15:59:59 -0400

    add database schema, render json in places controller

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,9 +1,10 @@
-class Api::V1::MonasteriesController < ApplicationController
+class Api::V1::PlacesController < ApplicationController
   def index
-
+    places = Place.all
+    render json: places
   end
 
   def show
-    
+
   end
 end
