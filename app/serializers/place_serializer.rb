37abec4acb6bcebc142b8acdab1c3dc364dae3f8
wkdewey <wkdewey@gmail.com>
2020-10-13 19:32:19 -0400

    add more custom serializer attributes

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -6,4 +6,12 @@ class PlaceSerializer
   attributes :percent_german do |place|
     ((place.german_pop.to_f / place.population.to_f) * 100).round(2)
   end
+
+  attributes :percent_african_american do |place|
+    ((place.af_am_pop.to_f / place.population.to_f) * 100).round(2)
+  end
+
+  attributes :percent_mexican do |place|
+    ((place.mexican_pop.to_f / place.population.to_f) * 100).round(2)
+  end
 end
