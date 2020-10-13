0622ae9ab05320e1a22bb189c8f6c228a52c6e43
wkdewey <wkdewey@gmail.com>
2020-10-13 19:22:04 -0400

    add custom serializer attribute for percent german

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -2,4 +2,8 @@ class PlaceSerializer
   include FastJsonapi::ObjectSerializer
 
   attributes :name, :population, :german_pop, :af_am_pop, :mexican_pop
+
+  attributes :percent_german do |place|
+    ((place.german_pop.to_f / place.population.to_f) * 100).round(2)
+  end
 end
