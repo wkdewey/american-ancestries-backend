b52a7b685c6e190e765c09887f02d8483d661a7b
wkdewey <wkdewey@gmail.com>
2020-10-18 18:25:56 -0400

    add serializer with custom attributes

---
+++ app/serializers/ancestry_group_serializer.rb
@@ -0,0 +1,10 @@
+class AncestryGroupSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :national_pop
+
+  @@us_pop = 328239523
+
+  attributes :national_percent do |group|
+    ((group.national_pop.to_f / @@us_pop.to_f) * 100).round(2)
+  end
+end
